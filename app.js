13044eb9da8e22bc3ea1675de468adec8bfde10a
Sonali Singh <sonalisingh.singh1299@gmail.com>
2025-08-27 22:47:52 +0530

    database-setup

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const connectDB = require('./config/db');
+connectDB();
 
 app.set('view engine', 'ejs');
 
--- config/db.js
+++ config/db.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 
 function connectDB() {
-    mongoose.connect('mongodb://
+    mongoose.connect(process.env.MONGO_URI).then(() => {
+        console.log("Connected to MongoDB");
+    })
 }
+
+module.exports = connectDB;
